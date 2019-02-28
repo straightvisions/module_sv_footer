f8bd18337bc1227717b6240d9579594b32335708
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-02-28 11:51:45 +0100

    Footer Redesign (done)

--- lib/tpl/frontend.php
+++ lib/tpl/frontend.php
@@ -1,18 +1,27 @@
 <footer class="<?php echo $this->get_prefix(); ?>">
-	<div class="<?php echo $this->get_prefix( 'main_bar' ); ?> sv_common_container">
-		<?php
-		echo do_shortcode( '[sv_sidebar template = "footer_left"]' );
-		echo do_shortcode( '[sv_sidebar template = "footer_center"]' );
-		echo do_shortcode( '[sv_sidebar template = "footer_right"]' );
-		?>
-	</div>
-	<div class="<?php echo $this->get_prefix( 'theme_bar' ); ?> sv_common_container">
-		<div class="<?php echo $this->get_prefix( 'theme_info' ); ?>">
-			<a href="https://straightvisions.com/de/">
-				<img src="https://media-straightvisions.com/2018/02/logo.svg" alt="straightvisions Logos">
-			</a>
-			<span>straightvisions 100-Theme - developed by <a href="https://straightvisions.com/de/">straightvisions</a></span>
+	<div class="sv_common_container">
+		<div class="<?php echo $this->get_prefix( 'widgets_bar' ); ?> ">
+			<?php
+			echo do_shortcode( '[sv_sidebar template = "footer_left"]' );
+			echo do_shortcode( '[sv_sidebar template = "footer_center"]' );
+			echo do_shortcode( '[sv_sidebar template = "footer_right"]' );
+			?>
 		</div>
-		<div class="<?php echo $this->get_prefix( 'theme_links' ); ?>"></div>
+		<div class="<?php echo $this->get_prefix( 'theme_bar' ); ?>">
+			<div class="<?php echo $this->get_prefix( 'theme_branding' ); ?>">
+				<a href="https://straightvisions.com/de/">
+					<img src="https://media-straightvisions.com/2018/02/logo.svg" alt="straightvisions Logos">
+				</a>
+			</div>
+			<div class="<?php echo $this->get_prefix( 'theme_info' ); ?>">
+				<span>straightvisions 100-Theme - developed by <a href="https://straightvisions.com/de/">straightvisions</a></span>
+			</div>
+			<div class="<?php echo $this->get_prefix( 'theme_links' ); ?>">
+				<a href="">Link 1</a>
+				<a href="">Link 2</a>
+				<a href="">Link 3</a>
+				<a href="">Link 4</a>
+			</div>
+		</div>
 	</div>
 </footer>

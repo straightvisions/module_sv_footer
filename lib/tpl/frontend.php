32d1e9b1cf6cd6f5307d469d98f8afd1c00cd31c
Matthias Reuter <matthias-reuter@straightvisions.com>
2019-03-06 19:54:19 +0100

    MIGRATE v2

--- lib/tpl/frontend.php
+++ lib/tpl/frontend.php
@@ -7,21 +7,5 @@
 			echo do_shortcode( '[sv_sidebar template = "footer_right"]' );
 			?>
 		</div>
-		<div class="<?php echo $this->get_prefix( 'theme_bar' ); ?>">
-			<div class="<?php echo $this->get_prefix( 'theme_branding' ); ?>">
-				<a href="https://straightvisions.com/de/">
-					<img src="https://media-straightvisions.com/2018/02/logo.svg" alt="straightvisions Logos">
-				</a>
-			</div>
-			<div class="<?php echo $this->get_prefix( 'theme_info' ); ?>">
-				<span>straightvisions 100-Theme - developed by <a href="https://straightvisions.com/de/">straightvisions</a></span>
-			</div>
-			<div class="<?php echo $this->get_prefix( 'theme_links' ); ?>">
-				<a href="">Link 1</a>
-				<a href="">Link 2</a>
-				<a href="">Link 3</a>
-				<a href="">Link 4</a>
-			</div>
-		</div>
 	</div>
 </footer>

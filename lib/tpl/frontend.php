f9fdf78022e6705db5696d512633b9c968215081
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-03-26 16:17:52 +0100

    Renamed shortcode setting template to id

--- lib/tpl/frontend.php
+++ lib/tpl/frontend.php
@@ -2,9 +2,9 @@
 	<div class="sv_common_container">
 		<div class="<?php echo $this->get_prefix( 'widgets_bar' ); ?> ">
 			<?php
-			echo do_shortcode( '[sv_sidebar template = "' . $this->get_module_name() . '_left"]' );
-			echo do_shortcode( '[sv_sidebar template = "' . $this->get_module_name() . '_center"]' );
-			echo do_shortcode( '[sv_sidebar template = "' . $this->get_module_name() . '_right"]' );
+			echo do_shortcode( '[sv_sidebar id = "' . $this->get_module_name() . '_left"]' );
+			echo do_shortcode( '[sv_sidebar id = "' . $this->get_module_name() . '_center"]' );
+			echo do_shortcode( '[sv_sidebar id = "' . $this->get_module_name() . '_right"]' );
 			?>
 		</div>
 	</div>

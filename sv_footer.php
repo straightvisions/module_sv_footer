9bf87f3931e234670d3b3570b9cf96c821c408f8
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-06-25 17:36:35 +0200

    FEAT - ADD - MAKE FOOTER OPTIN

--- sv_footer.php
+++ sv_footer.php
@@ -16,14 +16,35 @@ class sv_footer extends init {
 		// Module Info
 		$this->set_module_title( 'SV Footer' );
 		$this->set_module_desc( __( 'This module gives the ability to display the footer via the "[sv_footer]" shortcode.', 'straightvisions_100' ) );
-
-		$this->register_scripts()->register_sidebars();
 		
-		// Action Hooks & Filter
-		$this->is_first_load() ? add_action( 'wp_loaded', array( $this, 'add_widgets' ) ) : false;
+		// Section Info
+		$this->set_section_title( __( 'Footer', 'straightvisions_100' ) );
+		$this->set_section_desc( __( 'Settings', 'straightvisions_100' ) );
+		$this->set_section_type( 'settings' );
+		$this->get_root()->add_section( $this );
+		
+		$this->load_settings();
+		
+		if($this->s['activate']->run_type()->get_data() === '1') {
+			$this->register_scripts()->register_sidebars();
+			
+			// Action Hooks & Filter
+			$this->is_first_load() ? add_action( 'wp_loaded', array( $this, 'add_widgets' ) ) : false;
+		}
 	}
 	
-	public function add_widgets() {
+	protected function load_settings(): sv_footer {
+		$this->s['activate'] =
+			$this->get_setting()
+				 ->set_ID( 'activate' )
+				 ->set_title( __( 'Activate Footer', 'straightvisions_100' ) )
+				 ->set_description( __( 'Activate or deactivate the footer', 'straightvisions_100' ) )
+				 ->load_type( 'checkbox' );
+		
+		return $this;
+	}
+		
+		public function add_widgets() {
 		$this->get_root()->sv_sidebar
 			->clear_sidebar( 'sv_100_sv_sidebar_sv_footer_left' )
 			->clear_sidebar( 'sv_100_sv_sidebar_sv_footer_center' )
@@ -73,6 +94,10 @@ class sv_footer extends init {
 	}
 
 	public function load( $settings = array() ) :string {
+		if($this->s['activate']->run_type()->get_data() != '1') {
+			return '';
+		}
+		
 		$settings								= shortcode_atts(
 			array(
 				'inline'						=> false,

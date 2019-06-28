01838077127c36e366a2f5682ba2821dd0152d2d
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-06-28 16:14:19 +0200

    Updated module info

--- sv_footer.php
+++ sv_footer.php
@@ -1,134 +1,134 @@
 <?php
-namespace sv100;
-
-/**
- * @version         1.00
- * @author			straightvisions GmbH
- * @package			sv100
- * @copyright		2019 straightvisions GmbH
- * @link			https://straightvisions.com
- * @since			1.0
- * @license			See license.txt or https://straightvisions.com
- */
-
-class sv_footer extends init {
-	public function init() {
-		// Module Info
-		$this->set_module_title( 'SV Footer' );
-		$this->set_module_desc( __( 'This module gives the ability to display the footer via the "[sv_footer]" shortcode.', 'sv100' ) );
-		
-		// Section Info
-		$this->set_section_title( __( 'Footer', 'sv100' ) );
-		$this->set_section_desc( __( 'Settings', 'sv100' ) );
-		$this->set_section_type( 'settings' );
-		$this->get_root()->add_section( $this );
-		
-		$this->load_settings()->register_scripts()->register_sidebars();
-		
-		// Action Hooks & Filter
-		$this->is_first_load() ? add_action( 'wp_loaded', array( $this, 'add_widgets' ) ) : false;
-	}
+	namespace sv100;
 	
-	protected function load_settings(): sv_footer {
-		$this->s['activate'] =
-			$this->get_setting()
-				 ->set_ID( 'activate' )
-				 ->set_title( __( 'Activate Footer', 'sv100' ) )
-				 ->set_description( __( 'Activate or deactivate the footer.', 'sv100' ) )
-				 ->load_type( 'checkbox' );
+	/**
+	 * @version         4.000
+	 * @author			straightvisions GmbH
+	 * @package			sv100
+	 * @copyright		2019 straightvisions GmbH
+	 * @link			https://straightvisions.com
+	 * @since			1.000
+	 * @license			See license.txt or https://straightvisions.com
+	 */
+	
+	class sv_footer extends init {
+		public function init() {
+			// Module Info
+			$this->set_module_title( 'SV Footer' );
+			$this->set_module_desc( __( 'This module gives the ability to display the footer via the "[sv_footer]" shortcode.', 'sv100' ) );
+			
+			// Section Info
+			$this->set_section_title( __( 'Footer', 'sv100' ) );
+			$this->set_section_desc( __( 'Settings', 'sv100' ) );
+			$this->set_section_type( 'settings' );
+			$this->get_root()->add_section( $this );
+			
+			$this->load_settings()->register_scripts()->register_sidebars();
+			
+			// Action Hooks & Filter
+			$this->is_first_load() ? add_action( 'wp_loaded', array( $this, 'add_widgets' ) ) : false;
+		}
 		
-		return $this;
-	}
+		protected function load_settings(): sv_footer {
+			$this->s['activate'] =
+				$this->get_setting()
+					 ->set_ID( 'activate' )
+					 ->set_title( __( 'Activate Footer', 'sv100' ) )
+					 ->set_description( __( 'Activate or deactivate the footer.', 'sv100' ) )
+					 ->load_type( 'checkbox' );
+			
+			return $this;
+		}
 		
-	public function add_widgets() {
-		$this->get_module( 'sv_sidebar' )
-			 ->clear_sidebar( 'sv100_sv_sidebar_sv_footer_left' )
-			 ->clear_sidebar( 'sv100_sv_sidebar_sv_footer_center' )
-			 ->clear_sidebar( 'sv100_sv_sidebar_sv_footer_right' )
-			 ->add_widget_to_sidebar( 'recent-posts', 'sv100_sv_sidebar_sv_footer_left' )
-			 ->add_widget_to_sidebar( 'recent-comments', 'sv100_sv_sidebar_sv_footer_center' )
-			 ->add_widget_to_sidebar( 'meta', 'sv100_sv_sidebar_sv_footer_right' );
-	}
-
-	protected function register_scripts(): sv_footer {
-		// Register Styles
-		$this->scripts_queue['default'] =
-			static::$scripts->create( $this )
-							->set_ID( 'default' )
-							->set_path( 'lib/frontend/css/default.css' );
-
-		$this->scripts_queue['sidebar_default'] =
-			static::$scripts->create( $this )
-							->set_ID( 'sidebar_default' )
-							->set_path( 'lib/frontend/css/sidebar_default.css' );
-
-		return $this;
-	}
-
-	protected function register_sidebars(): sv_footer {
-		if ( $this->get_module( 'sv_sidebar' ) ) {
+		public function add_widgets() {
 			$this->get_module( 'sv_sidebar' )
-				->create( $this )
-				->set_ID( 'left' )
-				->set_title( __( 'Footer - Left', 'sv100' ) )
-				->set_desc( __( 'Widgets in this area will be shown in the left section of the footer.', 'sv100' ) )
-				->load_sidebar()
-				->create( $this )
-				->set_ID( 'center' )
-				->set_title( __( 'Footer - Center', 'sv100' ) )
-				->set_desc( __( 'Widgets in this area will be shown in the center section of the footer.', 'sv100' ) )
-				->load_sidebar()
-				->create( $this )
-				->set_ID( 'right' )
-				->set_title( __( 'Footer - Right', 'sv100' ) )
-				->set_desc( __( 'Widgets in this area will be shown in the right section of the footer.', 'sv100' ) )
-				->load_sidebar();
+				 ->clear_sidebar( 'sv100_sv_sidebar_sv_footer_left' )
+				 ->clear_sidebar( 'sv100_sv_sidebar_sv_footer_center' )
+				 ->clear_sidebar( 'sv100_sv_sidebar_sv_footer_right' )
+				 ->add_widget_to_sidebar( 'recent-posts', 'sv100_sv_sidebar_sv_footer_left' )
+				 ->add_widget_to_sidebar( 'recent-comments', 'sv100_sv_sidebar_sv_footer_center' )
+				 ->add_widget_to_sidebar( 'meta', 'sv100_sv_sidebar_sv_footer_right' );
 		}
-
-		return $this;
-	}
-
-	public function load( $settings = array() ): string {
-		if ( $this->get_setting( 'activate' )->run_type()->get_data() !== '1' ) {
-			return '';
+	
+		protected function register_scripts(): sv_footer {
+			// Register Styles
+			$this->scripts_queue['default'] =
+				static::$scripts->create( $this )
+								->set_ID( 'default' )
+								->set_path( 'lib/frontend/css/default.css' );
+	
+			$this->scripts_queue['sidebar_default'] =
+				static::$scripts->create( $this )
+								->set_ID( 'sidebar_default' )
+								->set_path( 'lib/frontend/css/sidebar_default.css' );
+	
+			return $this;
 		}
-		
-		$settings				= shortcode_atts(
-			array(
-				'inline'		=> false,
-			),
-			$settings,
-			$this->get_module_name()
-		);
-
-		return $this->router( $settings );
-	}
-
-	// Handles the routing of the templates
-	protected function router( array $settings ): string {
-		$template = array(
-			'name'      => 'default',
-			'scripts'   => array(
-				$this->scripts_queue[ 'default' ]->set_inline( $settings['inline'] ),
-				$this->scripts_queue[ 'sidebar_default' ]->set_inline( $settings['inline'] ),
-			),
-		);
-
-		return $this->load_template( $template, $settings );
-	}
-
-	// Loads the templates
-	protected function load_template( array $template, array $settings ) :string {
-		ob_start();
-		foreach ( $template['scripts'] as $script ) {
-			$script->set_is_enqueued();
+	
+		protected function register_sidebars(): sv_footer {
+			if ( $this->get_module( 'sv_sidebar' ) ) {
+				$this->get_module( 'sv_sidebar' )
+					->create( $this )
+					->set_ID( 'left' )
+					->set_title( __( 'Footer - Left', 'sv100' ) )
+					->set_desc( __( 'Widgets in this area will be shown in the left section of the footer.', 'sv100' ) )
+					->load_sidebar()
+					->create( $this )
+					->set_ID( 'center' )
+					->set_title( __( 'Footer - Center', 'sv100' ) )
+					->set_desc( __( 'Widgets in this area will be shown in the center section of the footer.', 'sv100' ) )
+					->load_sidebar()
+					->create( $this )
+					->set_ID( 'right' )
+					->set_title( __( 'Footer - Right', 'sv100' ) )
+					->set_desc( __( 'Widgets in this area will be shown in the right section of the footer.', 'sv100' ) )
+					->load_sidebar();
+			}
+	
+			return $this;
 		}
-
-		// Loads the template
-		include ( $this->get_path('lib/frontend/tpl/' . $template['name'] . '.php' ) );
-		$output							        = ob_get_contents();
-		ob_end_clean();
-
-		return $output;
+	
+		public function load( $settings = array() ): string {
+			if ( $this->get_setting( 'activate' )->run_type()->get_data() !== '1' ) {
+				return '';
+			}
+			
+			$settings				= shortcode_atts(
+				array(
+					'inline'		=> false,
+				),
+				$settings,
+				$this->get_module_name()
+			);
+	
+			return $this->router( $settings );
+		}
+	
+		// Handles the routing of the templates
+		protected function router( array $settings ): string {
+			$template = array(
+				'name'      => 'default',
+				'scripts'   => array(
+					$this->scripts_queue[ 'default' ]->set_inline( $settings['inline'] ),
+					$this->scripts_queue[ 'sidebar_default' ]->set_inline( $settings['inline'] ),
+				),
+			);
+	
+			return $this->load_template( $template, $settings );
+		}
+	
+		// Loads the templates
+		protected function load_template( array $template, array $settings ) :string {
+			ob_start();
+			foreach ( $template['scripts'] as $script ) {
+				$script->set_is_enqueued();
+			}
+	
+			// Loads the template
+			include ( $this->get_path('lib/frontend/tpl/' . $template['name'] . '.php' ) );
+			$output							        = ob_get_contents();
+			ob_end_clean();
+	
+			return $output;
+		}
 	}
-}

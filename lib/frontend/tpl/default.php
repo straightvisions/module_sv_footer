37e454a8d02201adc2b3c069b0020cdfaa521c9b
Matthias Bathke <info@straightvisions.com>
2019-07-05 12:19:35 +0200

    MAKE CREDIT LINK NOFOLLOW

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -19,7 +19,7 @@
 	<div class="<?php echo $this->get_prefix( 'credits_info' ); ?>">
 		<?php
 			echo '<span>' . __( 'Theme by', 'sv100' ) . '</span>';
-			echo wp_get_theme()->display( 'Author' );
+			echo '<a href="'.wp_get_theme()->display( 'AuthorURI' ).'" rel="nofollow" target="_blank">'.strip_tags(wp_get_theme()->display( 'Author' )).'</a>';
 		?>
 	</div>
 </div>

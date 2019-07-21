5c7cba47d4a6e95a62ef2854e5a0ed98dc3545cd
Matthias Bathke <info@straightvisions.com>
2019-07-21 12:30:23 +0200

    RE-ADD Footer Credits Filter

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -25,13 +25,7 @@
 		?>
 	</div>
 </footer>
-<?php } ?>
-
-<div class="<?php echo $this->get_prefix( 'credits' ); ?>">
-	<div class="<?php echo $this->get_prefix( 'credits_info' ); ?>">
-		<?php
-			echo '<span>' . __( 'Theme by', 'sv100' ) . '</span>';
-			echo '<a href="'.wp_get_theme()->display( 'AuthorURI' ).'" rel="nofollow" target="_blank">'.strip_tags(wp_get_theme()->display( 'Author' )).'</a>';
-		?>
-	</div>
-</div>
+<?php
+	}
+	include($this->get_path('lib/frontend/tpl/credits.php'));
+?>

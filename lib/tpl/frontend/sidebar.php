999cdf3b0dbd6e2fc6f0ac4462e0f80fb65e9638
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-03-25 15:38:10 +0100

    REPLACE WHITESPACE WITH TABS

--- lib/tpl/frontend/sidebar.php
+++ lib/tpl/frontend/sidebar.php
@@ -1,6 +1,6 @@
  <?php
-    for($i = 1; $i < 6; $i++){
-        echo $this->get_module( 'sv_sidebar' )
-            ? $this->get_module( 'sv_sidebar' )->load( $this->get_prefix($i) )
-            : '';
-    }
+	for($i = 1; $i < 6; $i++){
+		echo $this->get_module( 'sv_sidebar' )
+			? $this->get_module( 'sv_sidebar' )->load( $this->get_prefix($i) )
+			: '';
+	}
